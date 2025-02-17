79b6d33b6aacaa61ee53b3eeed19856a3c341243
wujie-bluefocus <jie.wu5@bluefocus.com>
2025-02-17 19:49:45 +0800

    add main debug testg

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/blueturbo-ad/go-utils/environment"
+	k8sclient "github.com/blueturbo-ad/go-utils/k8s_tool/k8s_client"
+	redisclient "github.com/blueturbo-ad/go-utils/redis_client"
+)
+
+func main() {
+	os.Setenv("POD_NAME", "test")
+	os.Setenv("POD_NAMESPACE", "dsp-ns")
+	environment.Init()
+	k8sclient.GetSingleton().SetUp()
+	var e = redisclient.GetSingleton()
+	err := e.UpdateLoadK8sConfigMap("redis-conf", "Dev")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	client := e.GetReadClient("event_redis")
+	if client == nil {
+		fmt.Println(err.Error())
+	}
+	ctx := context.Background()
+	res, err := client.Get(ctx, "test").Result()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(res)
+}
