2886936dab6226a11a2ba3766d99df048583a727
wujie-bluefocus <jie.wu5@bluefocus.com>
2025-02-18 16:19:35 +0800

    add main debug testg

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/blueturbo-ad/go-utils/environment"
 	gcpcloudstorage "github.com/blueturbo-ad/go-utils/gcp_cloud_tool/gcp_cloud_storage"
 	gcpcloudtool "github.com/blueturbo-ad/go-utils/gcp_cloud_tool/gcp_svc_acc_token"
+	redisclient "github.com/blueturbo-ad/go-utils/redis_client"
 )
 
 func main() {
@@ -27,21 +29,21 @@ func main() {
 	if err != nil {
 		fmt.Println("gcp access token", err.Error())
 	}
-	// var e = redisclient.GetSingleton()
+	var e = redisclient.GetSingleton()
 
-	// p := workPath + "/config/redis_conf.yaml"
-	// err = e.UpdateFromFile(p, "Dev")
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// client := e.GetReadClient("event_redis")
-	// if client == nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// ctx := context.Background()
-	// res, err := client.Get(ctx, "test").Result()
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-	// fmt.Println(res)
+	p := workPath + "/config/redis_conf.yaml"
+	err = e.UpdateFromFile(p, "Dev")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	client := e.GetReadClient("event_redis")
+	if client == nil {
+		fmt.Println(err.Error())
+	}
+	ctx := context.Background()
+	res, err := client.Get(ctx, "test").Result()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(res)
 }
