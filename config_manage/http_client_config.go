bd962176c1b8508a595b10ad067deb8a343a648f
wujie-bluefocus <jie.wu5@bluefocus.com>
2025-02-19 17:30:43 +0800

    add http_client

---
+++ config_manage/http_client_config.go
@@ -0,0 +1,82 @@
+package config_manage
+
+import (
+	"fmt"
+
+	"github.com/blueturbo-ad/go-utils/environment"
+	"gopkg.in/yaml.v3"
+)
+
+type Http1xConfig struct {
+	Name                string `yaml:"name"`
+	TimeOut             int64  `yaml:"timeout"`
+	KeepALive           int64  `yaml:"keep_alive"`
+	MaxIdleConns        int    `yaml:"max_idle_conns"`
+	MaxIdleConnsPerHost int    `yaml:"max_idle_conns_per_host"`
+	IdleConnTimeout     int64  `yaml:"idle_conn_timeout"`
+}
+
+type HttpClientConfig struct {
+	Config  *[]Http1xConfig `yaml:"http_conf"`
+	Version string          `yaml:"version"`
+}
+
+func (r *HttpClientConfig) LoadK8sConfigMap(configMapName, env string) error {
+	var c = new(ManagerConfig)
+	namespace := environment.GetSingleton().GetNamespace()
+	info, err := c.LoadK8sConfigMap(namespace, configMapName, env)
+	if err != nil {
+		return err
+	}
+	if (*info) == nil {
+		return fmt.Errorf("info is nil，")
+	}
+	inmap := (*info).(map[string]interface{})
+	data, err := yaml.Marshal(inmap)
+	if err != nil {
+		return fmt.Errorf("failed to marshal inmap: %v", err)
+	}
+	err = yaml.Unmarshal(data, &r)
+	if err != nil {
+		return fmt.Errorf(ErroryamlNotfound, err)
+	}
+	return nil
+}
+
+func (r *HttpClientConfig) LoadConfig(filePath string, env string) error {
+	var c = new(ManagerConfig)
+	info, err := c.LoadFileConfig(filePath, env)
+	if err != nil {
+		return err
+	}
+	//fmt.Println(info)
+	inmap := (*info).(map[string]interface{})
+	data, err := yaml.Marshal(inmap)
+	if err != nil {
+		return fmt.Errorf("failed to marshal inmap: %v", err)
+	}
+	err = yaml.Unmarshal(data, &r)
+	if err != nil {
+		return fmt.Errorf(ErroryamlNotfound, err)
+	}
+	return nil
+}
+
+func (l *HttpClientConfig) LoadMemoryConfig(buf []byte, env string) error {
+	var c = new(ManagerConfig)
+	info, err := c.LoadMemoryConfig(buf, env)
+	if err != nil {
+		return err
+	}
+	// 解析 YAML 数据
+	inmap := (*info).(map[string]interface{})
+	data, err := yaml.Marshal(inmap)
+	if err != nil {
+		return fmt.Errorf("failed to marshal inmap: %v", err)
+	}
+	err = yaml.Unmarshal(data, &l.Config)
+	if err != nil {
+		return fmt.Errorf(ErroryamlNotfound, err)
+	}
+	return nil
+}
---
+++ tcp_tool/http_client/http_client.go
@@ -0,0 +1,107 @@
+package httpclient
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/blueturbo-ad/go-utils/config_manage"
+)
+
+var (
+	instance    *HttpClientManager
+	once        sync.Once
+	EmptyString = ""
+)
+
+type HttpClientManager struct {
+	HttpClient [2]map[string]*http.Client
+
+	index   int
+	rwMutex sync.RWMutex
+}
+
+func GetSingleton() *HttpClientManager {
+	once.Do(func() {
+		instance = &HttpClientManager{
+			index: -1,
+		}
+
+	})
+	return instance
+}
+
+func (h *HttpClientManager) GetClient(name string) *http.Client {
+	h.rwMutex.RLock()
+	defer h.rwMutex.RUnlock()
+	if h.HttpClient[h.index][name] != nil {
+		return h.HttpClient[h.index][name]
+	}
+	return nil
+}
+
+func (h *HttpClientManager) UpdateLoadK8sConfigMap(configMapName, env string) error {
+	var e = new(config_manage.HttpClientConfig)
+	err := e.LoadK8sConfigMap(configMapName, env)
+	if err != nil {
+		return fmt.Errorf("redis client  LoadK8sConfigMap is error %s", err.Error())
+	}
+	return h.BuildClient(e)
+}
+
+// 函数用于内存更新etcd配置
+func (h *HttpClientManager) UpdateFromEtcd(env string, eventType string, key string, value string) error {
+	fmt.Printf("Event Type: %s, Key: %s, Value: %s\n", eventType, key, value)
+
+	var err error
+	switch key {
+	case "redis-conf":
+		var e = new(config_manage.HttpClientConfig)
+		err = e.LoadMemoryConfig([]byte(value), env)
+		if err != nil {
+			return err
+		}
+		if err := h.BuildClient(e); err != nil {
+			return err
+		}
+	default:
+		return nil
+	}
+	return nil
+}
+
+func (h *HttpClientManager) UpdateFromFile(confPath string, env string) error {
+	var err error
+	var e = new(config_manage.HttpClientConfig)
+	err = e.LoadConfig(confPath, env)
+	if err != nil {
+		return err
+	}
+	return h.BuildClient(e)
+}
+
+func (h *HttpClientManager) BuildClient(e *config_manage.HttpClientConfig) error {
+	h.rwMutex.Lock()
+	defer h.rwMutex.Unlock()
+	h.index = (h.index + 1) % 2
+	h.HttpClient[h.index] = make(map[string]*http.Client)
+
+	for _, v := range *e.Config {
+		client := http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   time.Duration(v.TimeOut) * time.Second,
+					KeepAlive: time.Duration(v.KeepALive) * time.Second,
+				}).DialContext,
+				MaxIdleConns:        v.MaxIdleConns,
+				MaxIdleConnsPerHost: v.MaxIdleConnsPerHost,
+				IdleConnTimeout:     time.Duration(v.IdleConnTimeout) * time.Second,
+			},
+		}
+		h.HttpClient[h.index][v.Name] = &client
+	}
+	return nil
+}
