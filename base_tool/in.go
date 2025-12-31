d35742d28d4a472917af250c06854c882a32627c
@jie.wu5 <jie.wu5@bluefocus.com>
2025-12-31 11:31:19 +0000

    update

--- base_tool/in.go
+++ base_tool/in.go
@@ -1,9 +1,7 @@
 package basetool
 
-
-
 // 基于基础类型的in操作
-func In[T string | int | int64 | int32 | float64 | float32, A []T](target T, arr A) bool {
+func In[T string | int | int64 | int32 | float64 | float32 | uint64 | uint32 | uint16, A []T](target T, arr A) bool {
 	for _, a := range arr {
 		if a == target {
 			return true
